dbefbe50ffcfaf7ebcbd897ed375026ff9c1c1b4
mark <lubegamark@gmail.com>
2016-07-28 13:35:29 +0300

    Add Basic API Documentation

--- books/viewsets.py
+++ books/viewsets.py
@@ -9,55 +9,88 @@ from books.serializers import UserSerializer, BookSerializer, ContributorSeriali
 
 
 class UserViewSet(ModelViewSet):
+    """
+    This endpoint presents the users
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class BookViewSet(ModelViewSet):
+    """
+    This Endpoint presents the books
+    """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
 class PublisherViewSet(ModelViewSet):
+    """
+    This Endpoint presents the publishers
+    """
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
 
 
 class ContributorViewSet(ModelViewSet):
+    """
+    This Endpoint presents the contributors
+    """
     queryset = Contributor.objects.all()
     serializer_class = ContributorSerializer
 
 
 class AuthorViewSet(ModelViewSet):
+    """
+    This Endpoint presents the authors
+    """
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
 
 
 class ReviewViewSet(ModelViewSet):
+    """
+    This Endpoint presents the Reviews
+    """
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
 class CategoryViewSet(ModelViewSet):
+    """
+    This Endpoint presents the categories
+    """
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
 class TagViewSet(ModelViewSet):
+    """
+    This Endpoint presents the tags
+    """
     queryset = Tag.objects.all()
     serializer_class = TagSerializer
 
 
 class SourceViewSet(ModelViewSet):
+    """
+    This Endpoint presents the sources
+    """
     queryset = Source.objects.all()
     serializer_class = SourceSerializer
 
 
 class FormatViewSet(ModelViewSet):
+    """
+    This Endpoint presents the formats
+    """
     queryset = Format.objects.all()
     serializer_class = FormatSerializer
 
 
 class LanguageViewSet(ModelViewSet):
+    """
+    This Endpoint presents the languages
+    """
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
