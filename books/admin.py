786ac25f846cb4313e6cbee88b5753c630360c52
Mark Lubega <lubegamark@gmail.com>
2016-08-10 19:15:44 +0300

    Add inline for availability in book admin

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,23 @@
 from auditlog.models import LogEntry
 from django.contrib import admin
 
-from books.models import Book, Source, Format, Language, Tag, Category, Review, Publisher, Contributor, Author
+from books.models import Book, Source, Format, Language, Tag, Category, Review, Publisher, Contributor, Author, \
+    BookSource
+
+
+class AuditLogAdmin(admin.ModelAdmin):
+    list_display = ('timestamp','content_type', 'object_repr', 'action', 'actor', 'remote_addr')
+    readonly_fields = ('content_type', 'object_pk', 'object_id', 'object_repr', 'action', 'changes', 'actor',
+                       'remote_addr', 'additional_data',)
+
+
+class BookSourceInline(admin.StackedInline):
+    model = BookSource
+    extra = 0
+
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [BookSourceInline, ]
 
 admin.site.register(Author)
 admin.site.register(Contributor)
@@ -12,14 +28,5 @@ admin.site.register(Tag)
 admin.site.register(Language)
 admin.site.register(Format)
 admin.site.register(Source)
-admin.site.register(Book)
-
-
-class AuditLogAdmin(admin.ModelAdmin):
-    list_display = ('timestamp','content_type', 'object_repr', 'action', 'actor', 'remote_addr')
-    readonly_fields = ('content_type', 'object_pk', 'object_id', 'object_repr', 'action', 'changes', 'actor',
-                       'remote_addr', 'additional_data',)
-    pass
-
-
+admin.site.register(Book, BookAdmin)
 admin.site.register(LogEntry, AuditLogAdmin)
